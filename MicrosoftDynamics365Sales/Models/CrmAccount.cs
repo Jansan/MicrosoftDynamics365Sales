eb740a1f172759840ed283189e5df66cdc102ca1
Jansan <jansan-26@hotmail.com>
2019-05-20 15:28:39 +0200

    Model created CrmAccount

---
+++ MicrosoftDynamics365Sales/Models/CrmAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MicrosoftDynamics365Sales.Models
+{
+    public class CrmAccount
+    {
+        public string UserIdEmail { get; set; }
+
+        public string Password { get; set; }
+
+        public string CrmWebServiceUrl { get; set; }
+
+    }
+}
