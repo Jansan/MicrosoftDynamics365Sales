e6cf91d3ed11ccde6d7355a6cb94f3365b360cb5
Jansan <jansan-26@hotmail.com>
2019-05-20 15:35:05 +0200

    CrmAccount Data Annotations added

--- MicrosoftDynamics365Sales/Models/CrmAccount.cs
+++ MicrosoftDynamics365Sales/Models/CrmAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,19 @@ namespace MicrosoftDynamics365Sales.Models
 {
     public class CrmAccount
     {
+        [Display(Name ="User Name")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage ="UserId email is required")]
         public string UserIdEmail { get; set; }
 
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
 
+        [Display(Name = "CRM Web service")]
+        [DataType(DataType.Url)]
+        [Required(ErrorMessage = "CRM Web service URL is required")]
         public string CrmWebServiceUrl { get; set; }
 
     }
