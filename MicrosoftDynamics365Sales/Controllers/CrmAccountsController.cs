198501f0fff5f046a0e59c51cf01cf9c51d965a4
jansan <jansan-26@hotmail.com>
2019-05-21 09:40:36 +0200

    CrmAccounts Controller create

---
+++ MicrosoftDynamics365Sales/Controllers/CrmAccountsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MicrosoftDynamics365Sales.Controllers
+{
+    public class CrmAccountsController : Controller
+    {
+        // GET: CrmAccounts
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
