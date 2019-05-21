792f9d6e511df63db5766e4e64b253d811f0f877
jansan <jansan-26@hotmail.com>
2019-05-21 13:01:07 +0200

    AccountViewModel created

---
+++ MicrosoftDynamics365Sales/ViewModels/AccountViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MicrosoftDynamics365Sales.ViewModels
+{
+    public class AccountViewModel
+    {
+        public Guid AccountId { get; set; }
+
+        [StringLength(255)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Account Name")]
+        [Required(ErrorMessage = "Account Name is required")]
+        public string AccountName { get; set; }
+
+        [StringLength(255)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Account Number")]
+        [Required(ErrorMessage = "AccountNumber is required")]
+        public string AccountNumber { get; set; }
+
+        [StringLength(255)]
+        [DataType(DataType.Text)]
+        [Display(Name = "VatNumber")]
+        [Required(ErrorMessage = "VatNumber is required")]
+        public string VatNumber { get; set; }
+    }
+}
