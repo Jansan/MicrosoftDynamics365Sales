1fabc073f7df24284e807a58c6777ff6c30cfe6b
jansan <jansan-26@hotmail.com>
2019-05-21 10:11:09 +0200

    CrmAccountViewModel created

---
+++ MicrosoftDynamics365Sales/ViewModels/CrmAccountViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MicrosoftDynamics365Sales.ViewModels
+{
+    public class CrmAccountViewModel
+    {
+        [Display(Name = "User Name")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "UserId email is required")]
+        public string UserIdEmail { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Display(Name = "CRM Web service")]
+        [DataType(DataType.Url)]
+        [Required(ErrorMessage = "CRM Web service URL is required")]
+        public string CrmWebServiceUrl { get; set; }
+    }
+}
