de7c2130c113e15d94618213d1579bb75ca2ce81
jansan <jansan-26@hotmail.com>
2019-05-21 15:11:03 +0200

    Accountscontroller created

---
+++ MicrosoftDynamics365Sales/Controllers/AccountsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MicrosoftDynamics365Sales.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
