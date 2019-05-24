9cf5983fba2560dfd111454752a48d573bcee00d
jansan <jansan-26@hotmail.com>
2019-05-24 14:29:14 +0200

    accounts index view and details view created

--- MicrosoftDynamics365Sales/Controllers/AccountsController.cs
+++ MicrosoftDynamics365Sales/Controllers/AccountsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dynamics365SalesTest.DAL;
+using MicrosoftDynamics365Sales.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,20 @@ namespace MicrosoftDynamics365Sales.Controllers
         // GET: Accounts
         public ActionResult Index()
         {
-            return View();
+            DAL_AccountEntity objDAL = new DAL_AccountEntity();
+            List<AccountViewModel> accountinfo = objDAL.RetriveRecords();
+
+            return View(accountinfo);
+        }
+
+        // GET: Account/Details
+        public ActionResult Details(Guid id)
+        {
+            DAL_AccountEntity objDAL = new DAL_AccountEntity();
+            List<AccountViewModel> accountinfo = objDAL.RetriveRecords();
+            var account = accountinfo.Where(a => a.AccountId == id).FirstOrDefault();
+
+            return View(account);
         }
     }
 }
