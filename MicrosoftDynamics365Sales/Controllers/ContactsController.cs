f2dca2034f2624c6400de5ec997e8ff1d5698799
jansan <jansan-26@hotmail.com>
2019-05-24 15:57:18 +0200

    contactscontroller created

---
+++ MicrosoftDynamics365Sales/Controllers/ContactsController.cs
@@ -0,0 +1,22 @@
+﻿using MicrosoftDynamics365Sales.DAL;
+using MicrosoftDynamics365Sales.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MicrosoftDynamics365Sales.Controllers
+{
+    public class ContactsController : Controller
+    {
+        // GET: Contacts
+        public ActionResult Index()
+        {
+            DAL_ContactEntity objDAL = new DAL_ContactEntity();
+            List<ContactViewModel> contactinfo = objDAL.RetriveRecords(); 
+          
+            return View(contactinfo);
+        }
+    }
+}
