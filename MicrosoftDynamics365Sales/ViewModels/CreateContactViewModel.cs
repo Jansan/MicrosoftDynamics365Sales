9e44e268e955087a7e2a184b9193a2157c013ee6
Jansan <jansan-26@hotmail.com>
2019-06-10 13:42:41 +0200

    createcontactviewmodel created

---
+++ MicrosoftDynamics365Sales/ViewModels/CreateContactViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MicrosoftDynamics365Sales.ViewModels
+{
+    public class CreateContactViewModel
+    {
+        [StringLength(255)]
+        [DataType(DataType.Text)]
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "FirstName is required")]
+        public string FirstName { get; set; }
+
+        [StringLength(255)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "LastName is required")]
+        public string LastName { get; set; }
+
+        [StringLength(255)]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [StringLength(255)]
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "Mobile is required")]
+        public string Mobile { get; set; }
+    }
+}
