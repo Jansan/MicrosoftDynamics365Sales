36ab0ab921f598757836f3ee9da3f0cf0601d8ad
Jansan <jansan-26@hotmail.com>
2019-06-10 17:03:44 +0200

    validate modefied

--- MicrosoftDynamics365Sales/ViewModels/CreateContactViewModel.cs
+++ MicrosoftDynamics365Sales/ViewModels/CreateContactViewModel.cs
@@ -8,24 +8,26 @@ namespace MicrosoftDynamics365Sales.ViewModels
 {
     public class CreateContactViewModel
     {
-        [StringLength(255)]
+        [StringLength(255,ErrorMessage = "First name has to be under 255 characters long")]
         [DataType(DataType.Text)]
         [Display(Name = "First Name")]
         [Required(ErrorMessage = "FirstName is required")]
         public string FirstName { get; set; }
 
-        [StringLength(255)]
+        [StringLength(255,ErrorMessage = "Last name has to be under 255 characters long")]
         [DataType(DataType.Text)]
         [Display(Name = "Last Name")]
         [Required(ErrorMessage = "LastName is required")]
         public string LastName { get; set; }
 
-        [StringLength(255)]
+        [StringLength(255,ErrorMessage = "Email has to be under 255 characters long")]
+        [EmailAddress(ErrorMessage ="Please enter a valid email address")]
         [DataType(DataType.EmailAddress)]
         [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
 
-        [StringLength(255)]
+        [StringLength(13,ErrorMessage ="Mobile has to be under 13 numbers long")]
+        [Phone(ErrorMessage = "Please enter a valid mobile number")]
         [DataType(DataType.PhoneNumber)]
         [Required(ErrorMessage = "Mobile is required")]
         public string Mobile { get; set; }
