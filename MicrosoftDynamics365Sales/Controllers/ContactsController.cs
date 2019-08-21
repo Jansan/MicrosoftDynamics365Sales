a3092e68ef3c142cd9bd87326d71ce1db55cbda0
Jansan <jansan-26@hotmail.com>
2019-08-21 12:46:18 +0200

    contact page Authorize

--- MicrosoftDynamics365Sales/Controllers/ContactsController.cs
+++ MicrosoftDynamics365Sales/Controllers/ContactsController.cs
@@ -15,85 +15,105 @@ namespace MicrosoftDynamics365Sales.Controllers
         // GET: Contacts
         public ActionResult Index()
         {
-            DAL_ContactEntity objDAL = new DAL_ContactEntity();
-            List<ContactViewModel> contactinfo = objDAL.RetriveRecords(); 
+
+            if(Session["userId"] != null)
+            {
+                DAL_ContactEntity objDAL = new DAL_ContactEntity();
+                List<ContactViewModel> contactinfo = objDAL.RetriveRecords();
+                return View(contactinfo);
+            }
+            return RedirectToAction("Login", "CrmAccounts");
           
-            return View(contactinfo);
+           
         }
 
         //EDIT Contact
         [HttpPost]
         public ActionResult EditContact(Guid id, string propertyName, string value)
         {
-            var status = false;
-            var message = "";
-
-            //Update data to CRM
-            DAL_ContactEntity objDAL = new DAL_ContactEntity();
-            List<ContactViewModel> contacts = objDAL.RetriveRecords();
-
-            var contact = contacts.Where(c => c.ContactId == id).FirstOrDefault();
-            
-            if(contact != null)
+            if(Session["userId"] != null)
             {
-                if (propertyName == "FirstName")
-                {
-                    contact.FirstName = value;
-                }
-                if (propertyName == "Mobile")
-                {
-                    contact.Mobile = value;
-                }
-                objDAL.UpdateRecord(contact);
-                status = true;
-            }
-            else
-            {
-                message = "Error!";
-            }
-            
-            
+                var status = false;
+                var message = "";
 
-            var response = new { value = value, status = status, message = message };
-            JObject o = JObject.FromObject(response);
+                //Update data to CRM
+                DAL_ContactEntity objDAL = new DAL_ContactEntity();
+                List<ContactViewModel> contacts = objDAL.RetriveRecords();
 
-            return Content(o.ToString());
+                var contact = contacts.Where(c => c.ContactId == id).FirstOrDefault();
+
+                if (contact != null)
+                {
+                    if (propertyName == "FirstName")
+                    {
+                        contact.FirstName = value;
+                    }
+                    if (propertyName == "Mobile")
+                    {
+                        contact.Mobile = value;
+                    }
+                    objDAL.UpdateRecord(contact);
+                    status = true;
+                }
+                else
+                {
+                    message = "Error!";
+                }
+
+
+
+                var response = new { value = value, status = status, message = message };
+                JObject o = JObject.FromObject(response);
+
+                return Content(o.ToString());
+            }
+            return RedirectToAction("Login", "CrmAccounts");
+
         }
 
-        //Add New Contact
+        //Add New Contact Form
         public ActionResult CreateContact()
         {
-            return View();
+            if(Session["userId"] != null)
+            {
+                return View();
+            }
+            return RedirectToAction("Login", "CrmAccounts");
+            
         }
-
+        //Add New Contact
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult CreateContact(CreateContactViewModel vm)
         {
-            if (!ModelState.IsValid)
+            if(Session["userId"] != null)
             {
-                return View(vm);
+                if (!ModelState.IsValid)
+                {
+                    return View(vm);
+                }
+                DAL_ContactEntity objDAL = new DAL_ContactEntity();
+                objDAL.PostRecord(vm);
+
+                return RedirectToAction(nameof(Index));
             }
-            DAL_ContactEntity objDAL = new DAL_ContactEntity();
-            objDAL.PostRecord(vm);
-
-            return RedirectToAction(nameof(Index));
-
-            
-
+            return RedirectToAction("Login", "CrmAccounts");
+          
         }
 
         //Delete Contact
         [HttpPost]
-        //[ValidateAntiForgeryToken]
+        [ValidateAntiForgeryToken]
         public ActionResult DeleteContact(Guid contactId)
         {
-            
+            if(Session["userId"] != null)
+            {
                 DAL_ContactEntity objDAL = new DAL_ContactEntity();
                 objDAL.DeleteRecord(contactId);
 
-                return RedirectToAction("Index","Contacts");
-                    
+                return RedirectToAction("Index", "Contacts");
+            }
+            return RedirectToAction("Login", "CrmAccounts");
         }
 
 
